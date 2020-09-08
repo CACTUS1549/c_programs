991e8fe349171ec2e18cc1580fee6bba19b48e08
deepakreddy <deepak.jonnalagadda@gmail.com>
2020-09-08 19:28:45 +0530

    Problem 3 solved

--- chapter_1/prob_3/main.c
+++ chapter_1/prob_3/main.c
@@ -13,28 +13,59 @@ in each subject is 100.
 
 
 #include <stdio.h>
+#include <stdbool.h>
+
+int get_marks();
+
+#define max_marks				100
+#define min_marks				0
+#define max_total_marks			500
+
+int math_marks, physics_marks, chem_marks, bio_marks, english_marks, aggregate_marks = 0;
+float marks_percentage = 0.0;
+
 
 int main(){
 	
-	#define max_marks				100
-	#define min_marks				0
+	 math_marks = get_marks();
+	 physics_marks = get_marks();
+	 chem_marks = get_marks();
+	 bio_marks = get_marks();
+	 english_marks = get_marks();
 
-	int aggreg_marks, math_marks, physics_marks, chemistry_marks, bio_marks, eng_marks;
-	float percentage;
-	int temp_marks;
+	 aggregate_marks = math_marks + physics_marks + chem_marks + bio_marks + english_marks;
+	 marks_percentage = (aggregate_marks * 100) / max_total_marks;
 
-	printf("Enter Math marks\n");
-	scanf("%d", &temp_marks);
-	if (temp_marks >= min_marks && temp_marks <= max_marks)
-	{	
-		printf("Correct marks entered\n");
-		math_marks = temp_marks;
-	} else{
-		printf("Math marks are out of range. Please enter between 0-100\n");
-		scanf("%d", &temp_marks);
+	 printf("Aggregate marks: %d\n", aggregate_marks);
+	 printf("Marks percentage: %.2f\n", marks_percentage);
+
+	return 0;
+}
+
+int get_marks(void){
+	bool is_wrong_marks_entered = false;
+	int entered_marks = 0;
+
+	if (!is_wrong_marks_entered)
+	{
+		printf("Enter marks:\n");
+		scanf("%d", &entered_marks);	
 	}
 
-	printf("if-else exited.\n");
-	printf("%d\n", math_marks);
-	return 0;
+	do
+	{
+
+		if (entered_marks >=0 && entered_marks <=100)
+		{
+			is_wrong_marks_entered = false;
+		}
+
+		else{
+			printf("Marks out of range. Enter correct marks:\n");
+			scanf("%d", &entered_marks);
+			is_wrong_marks_entered = true;
+		}
+	} while (is_wrong_marks_entered);
+
+	return entered_marks;
 }
